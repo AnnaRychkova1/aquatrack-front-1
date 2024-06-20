81fc0750e7899b56f2a10216b46f4db4288665ca
AnnaRychkova1 <aanytkaa@gmail.com>
2024-06-20 22:24:56 +0100

    svg and png

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,8 +1,9 @@
-import sprite from "/src/assets/images/svg/sprite.svg";
+// import sprite from "/src/assets/images/svg/sprite.svg";
+import sprite from "/src/assets/images/svg/symbol-defs.svg";
 
-const Iconsvg = ({ width, height, iconName, styles }) => {
+const Iconsvg = ({ width, height, iconName, className }) => {
   return (
-    <svg width={width} height={height} className={styles}>
+    <svg width={width} height={height} className={className}>
       <use href={`${sprite}#${iconName}`}></use>
     </svg>
   );
