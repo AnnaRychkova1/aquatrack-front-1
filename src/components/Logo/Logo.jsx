5782c8a6bf3e6c0084dd48aca6875fa7802beef1
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-21 15:50:36 +0300

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,17 +1,20 @@
 // import { Link } from "react-router-dom";
-
 // const Logo = () => {
 //   return <Link to="/">AquaTrack</Link>;
 // };
-
 // export default Logo;
 
+// import { Link } from "react-router-dom";
+// import css from "./Logo.module.css";
+
+// export const Logo = () => {
+//   <Link to="/" className={css.logoFont}>
+//     AQUATRACK
+//   </Link>;
+// };
+
 import { Link } from "react-router-dom";
-//import css from "./Logo.module.css";
 
 export const Logo = () => {
-  <Link to="/">AquaTrack</Link>;
-  // <Link to="/" className={css.logoFont}>
-  //   AQUATRACK
-  // </Link>;
+  return <Link to="/">AQUATRACK</Link>;
 };
