5efa323a4809d9845c7d7acb1321bfc8ed3daf3d
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-21 14:35:37 +0300

    Update WelcomeSection.jsx

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -13,3 +13,29 @@
 // };
 
 // export default WelcomeSection;
+
+import { Link } from "react-router-dom";
+
+import { Logo } from "../Logo/Logo";
+import css from "./WelcomeSection.module.css";
+
+export const WelcomeSection = () => {
+  return (
+    <div className={css.welcomeBox}>
+      <Logo />
+      <div>
+        <p className={css.welcomePhrase}>Record daily water intake and track</p>
+        <h1 className={css.welcomeName}>Water consumption tracker</h1>
+        <div className={css.welcomeButton_block}>
+          <Link to="/signup" className={css.welcomeButton_tracker}>
+            Try tracker
+          </Link>
+
+          <Link to="/signin" className={css.welcomeButton_sign}>
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
