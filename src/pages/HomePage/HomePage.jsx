d14aa870996baf12f4f77f3f168addf657ae2a75
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-21 14:38:27 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,29 @@
-import { Helmet } from "react-helmet-async";
-import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
-import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
+// import { Helmet } from "react-helmet-async";
+// import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
+// import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 
-const Home = () => {
+// const Home = () => {
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Home</title>
+//       </Helmet>
+//       <WelcomeSection />
+//       <AdvantagesSection />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import { Helmet } from "react-helmet-async";
+import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
+import { WelcomeSection } from "../../components/WelcomeSection/WelcomeSection";
+import css from "./HomePage.module.css";
+
+export default function HomePage() {
   return (
-    <div>
+    <div className={css.generalHomePage}>
       <Helmet>
         <title>Home</title>
       </Helmet>
@@ -12,6 +31,4 @@ const Home = () => {
       <AdvantagesSection />
     </div>
   );
-};
-
-export default Home;
+}
