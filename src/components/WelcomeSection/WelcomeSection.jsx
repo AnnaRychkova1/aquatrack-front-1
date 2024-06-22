b432fc0bc2dd11b428874dd7372229d4ab64b9bb
AnnaRychkova1 <aanytkaa@gmail.com>
2024-06-22 15:23:01 +0100

    navig

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,22 +1,6 @@
-// import Logo from "../Logo/Logo";
-// import { Navigation } from "../Navigation/Navigation";
-
-// const WelcomeSection = () => {
-//   return (
-//     <section>
-//       <Logo />
-//       <h1>Home page</h1>
-//       <p>WelcomeSection</p>
-//       <Navigation />
-//     </section>
-//   );
-// };
-
-// export default WelcomeSection;
-
-import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import css from './WelcomeSection.module.css';
+import { Navigation } from '../Navigation/Navigation';
 
 const WelcomeSection = () => {
   return (
@@ -25,15 +9,7 @@ const WelcomeSection = () => {
       <div>
         <p className={css.welcomePhrase}>Record daily water intake and track</p>
         <h1 className={css.welcomeName}>Water consumption tracker</h1>
-        <div className={css.welcomeButton_block}>
-          <Link to="/signup" className={css.welcomeButton_tracker}>
-            Try tracker
-          </Link>
-
-          <Link to="/signin" className={css.welcomeButton_sign}>
-            Sign In
-          </Link>
-        </div>
+        <Navigation />
       </div>
     </div>
   );
