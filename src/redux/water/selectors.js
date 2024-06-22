b199b2b6a8839d2bb0cdec91ca06ea634bc3901a
Yuriy2223 <yuriy.shukan@gmail.com>
2024-06-22 21:00:01 +0300

    Update selectors.js

---
+++ src/redux/water/selectors.js
@@ -0,0 +1,5 @@
+export const selectLoading = (state) => state.waters.loading;
+
+export const selectFilter = (state) => state.waters.filter;
+
+export const selectWaters = (state) => state.waters.items;
