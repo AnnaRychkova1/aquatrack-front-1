3705cb4540df4675eaa2d015a4d73ee70a500aa5
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-22 16:20:02 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,9 @@
-// import { Helmet } from "react-helmet-async";
-// import WelcomeSection from "../../components/WelcomeSection/WelcomeSection";
-// import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Helmet>
-//         <title>Home</title>
-//       </Helmet>
-//       <WelcomeSection />
-//       <AdvantagesSection />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import { Helmet } from "react-helmet-async";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import { WelcomeSection } from "../../components/WelcomeSection/WelcomeSection";
 import css from "./HomePage.module.css";
 
-export default function HomePage() {
+const HomePage = () => {
   return (
     <div className={css.generalHomePage}>
       <Helmet>
@@ -31,4 +13,6 @@ export default function HomePage() {
       <AdvantagesSection />
     </div>
   );
-}
+};
+
+export default HomePage;
