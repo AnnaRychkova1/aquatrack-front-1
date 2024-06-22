5c3b507de3544aeb7ce483d377e71c58cff25859
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-22 13:52:06 +0300

    clear code

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,39 +1,9 @@
-// const AdvantagesSection = () => {
-//   return <section>AdvantagesSection</section>;
-// };
-
-// export default AdvantagesSection;
-
 import css from "./AdvantagesSection.module.css";
-// import user_1 from "../../assets/images/png/customer-1.png";
-// import user_2 from "../../assets/images/png/customer-2.png";
-// import user_3 from "../../assets/images/png/customer-3.png";
-
-// import user_1 from "../../assets/images/png/customerbig-1.png";
-// import user_2 from "../../assets/images/png/customerbig-2.png";
-// import user_3 from "../../assets/images/png/customerbig-3.png";
 
 export const AdvantagesSection = () => {
   return (
     <div className={css.advantages_main}>
       <div className={css.advantages_users}>
-        {/* <ul className={css.advantages_usersImg}>
-          <li className={css.advantages_point}>
-            <div className={css.user}>
-
-            </div>
-            <img className={css.user} src={user_1} alt="user1" />
-          </li>
-
-          <li className={css.advantages_point}>
-            <img className={css.user} src={user_2} alt="user2" />
-          </li>
-
-          <li className={css.advantages_point}>
-            <img className={css.user} src={user_3} alt="user3" />
-          </li>
-        </ul> */}
-
         <ul className={css.advantages_usersImg}>
           <li className={css.advantages_point}>
             <div className={`${css.user} ${css.user1}`}></div>
