4a69b3a234b796639aa639f92939383112a38013
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-23 14:16:21 +0300

    Update SignInForm.jsx

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,5 +1,196 @@
-const SignInForm = () => {
-  return <div>SignInForm</div>;
+// const SignInForm = () => {
+//   return <div>SignInForm</div>;
+// };
+
+// export default SignInForm;
+
+// import css from "./SignInForm.module.css";
+// import { NavLink } from "react-router-dom";
+// import Logo from "../Logo/Logo";
+// import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import { useForm } from "react-hook-form";
+// import { useDispatch } from "react-redux";
+
+// import { loginUser } from "../../redux/user/userOperations";
+// import { useState } from "react";
+// import sprite from "../../assets/images/svg/sprite.svg";
+// import toast from "react-hot-toast";
+
+// const schema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .email("Please enter valid email")
+//     .required("Email field is required"),
+//   password: yup
+//     .string()
+//     .required("Password field is required")
+//     .min(6, "Password must contain at least 6 characters"),
+// });
+
+// const SignInForm = () => {
+//   const {
+//     register,
+//     handleSubmit,
+
+//     formState: { errors },
+//     reset,
+//   } = useForm({
+//     resolver: yupResolver(schema),
+//     mode: "onChange",
+//     defaultValues: {
+//       email: "",
+//       password: "",
+//     },
+//   });
+//   const dispatch = useDispatch();
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   return (
+//     <div className={css.loginContainer}>
+//       {<Logo />}
+//       <div className={css.formContainer}>
+//         <h1 className={css.title}>Sign In</h1>
+//         <form
+//           className={css.form}
+//           onSubmit={handleSubmit(async (data) => {
+//             try {
+//               const resultAction = await dispatch(loginUser(data));
+
+//               if (loginUser.fulfilled.match(resultAction)) {
+//                 toast.success("You were successfully signed in!");
+//                 reset();
+//               } else if (loginUser.rejected.match(resultAction)) {
+//                 toast.error("Something went wrong. Please try again.");
+//               }
+//             } catch (error) {
+//               toast.error("Unexpected error. Please try again.");
+//             }
+//           })}
+//         >
+//           <label className={css.label}>Email</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.email ? css.error : ""}`}
+//               required={true}
+//               type="email"
+//               {...register("email", {
+//                 pattern: {
+//                   value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+//                   message: "Please enter valid email",
+//                 },
+//               })}
+//               placeholder="Enter your email"
+//               autoComplete="on"
+//             />
+//             {errors.email && (
+//               <span className={css.errors}>{errors.email.message}</span>
+//             )}
+//           </div>
+//           <label className={css.label}>Password</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.password ? css.error : ""}`}
+//               required={true}
+//               type={showPassword ? "text" : "password"}
+
+// type={"password"}
+
+//               {...register("password")}
+//               placeholder="Enter your Password"
+//             />
+//             {errors.password && (
+//               <span className={css.errors}>{errors.password.message}</span>
+//             )}
+//             {!showPassword && (
+//               <svg
+//                 className={css.icon_eye}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#icon-eye-off`}
+//                 ></use>
+//               </svg>
+//             )}
+
+//             {showPassword && (
+//               <svg
+//                 className={css.eyeIconOff}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#icon-eye`}
+//                 ></use>
+//               </svg>
+//             )}
+//           </div>
+
+//           <button className={css.button} type="submit">
+//             Sign In
+//           </button>
+//         </form>
+//         <p className={css.description}>
+//           Don’t have an account?&nbsp;
+//           <NavLink className={css.link} to={"/signup"}>
+//             Sign Up
+//           </NavLink>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+//export default SignInForm;
+
+import React from 'react';
+import '../SignInForm/SignInForm.module.css';
+
+const SignIn = () => {
+  return (
+    <div className="loginContainer">
+      <div className="formContainer">
+        <h1 className="title">Sign In</h1>
+        <form className="form">
+          <label className="label">Email</label>
+          <div className="input_field">
+            <input
+              className="input"
+              type="email"
+              placeholder="Enter your email"
+              autoComplete="on"
+            />
+          </div>
+          <label className="label">Password</label>
+          <div className="input_field">
+            <input
+              className="input"
+              type="password"
+              placeholder="Enter your Password"
+            />
+            <svg className="icon_eye">
+              <use
+                width={20}
+                height={20}
+                // xlinkHref={`${sprite}#icon-eye-off`}
+              ></use>
+            </svg>
+          </div>
+          <button className="button" type="submit">
+            Sign In
+          </button>
+        </form>
+        <p className="description">
+          Don’t have an account?&nbsp;
+          {/* <NavLink className="link" to={"/signup"}>
+            Sign Up
+          </NavLink> */}
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default SignInForm;
+export default SignIn;
