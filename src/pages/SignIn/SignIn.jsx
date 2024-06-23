e7110150019e64df97d349d24c7d1ef2014cb065
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-23 14:11:57 +0300

    add pages signIn

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,33 @@
-import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
-import Logo from "../../components/Logo/Logo";
-import SignInForm from "../../components/SignInForm/SignInForm";
+// import { Helmet } from "react-helmet-async";
+// import { Link } from "react-router-dom";
+// import Logo from "../../components/Logo/Logo";
+// import SignInForm from "../../components/SignInForm/SignInForm";
+
+// const SignIn = () => {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>SignIn</title>
+//       </Helmet>
+//       <Logo />
+//       <SignInForm />
+//       <p>
+//         Don’t have an account?
+//         <Link to="/signup">Sign Up</Link>
+//       </p>
+//     </>
+//   );
+// };
+
+// export default SignIn;
+
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import SignInForm from '../../components/SignInForm/SignInForm';
+import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
+import css from './SignInPage.module.css';
 
 const SignIn = () => {
   return (
@@ -10,11 +36,12 @@ const SignIn = () => {
         <title>SignIn</title>
       </Helmet>
       <Logo />
-      <SignInForm />
-      <p>
-        Don’t have an account?
-        <Link to="/signup">Sign Up</Link>
-      </p>
+      <div className={css.generalSignInForm}>
+        <SignInForm />
+        <div className={css.AdvantagesSection_plus}>
+          <AdvantagesSection />
+        </div>
+      </div>
     </>
   );
 };
