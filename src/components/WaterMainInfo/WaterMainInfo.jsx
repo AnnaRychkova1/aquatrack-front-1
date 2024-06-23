8f4ca616d0e03a2e251d6bb8cbe653114f547898
Agletty <agletty.lu@gmail.com>
2024-06-24 00:46:19 +0300

    WaterMainInfo add style

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,34 +1,19 @@
-// import css from "./WaterMainInfo.module.css";
-
-// import Logo from "../../components/Logo/Logo";
-// import AddWaterBtn from "./AddWaterBtn/AddWaterBtn";
-// import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma";
-// import WaterProgressBar from "./WaterProgressBar/WaterProgressBar";
-
-// const WaterMainInfoSection = () => {
-//   return (
-//     <div className={css.infoContainer}>
-//       <Logo />
-//       <div className={css.imgInfoContainer}></div>
-
-import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
-import Logo from '../Logo/Logo';
-import WaterDailyNorma from '../WaterDailyNorma/WaterDailyNorma';
-import WaterProgressBar from '../WaterProgressBar/WaterProgressBar';
+import WaterDailyNorma from './WaterDailyNorma/WaterDailyNorma';
+import WaterProgressBar from './WaterProgressBar/WaterProgressBar';
 import css from './WaterMainInfo.module.css';
+import AddWaterMainInfoBtn from './AddWaterMainInfoBtn/AddWaterMainInfoBtn';
+import LogoTracker from './LogoTracker/LogoTracker';
 
 const WaterMainInfo = () => {
   return (
     <div className={css.trackContainerItem}>
-      <Logo />
-
+      <LogoTracker />
+      <div className={css.imgInfoContainer}></div>
       <WaterDailyNorma />
       <WaterProgressBar />
-      <AddWaterBtn />
+      <AddWaterMainInfoBtn />
     </div>
   );
 };
-
-// export default WaterMainInfoSection;
 
 export default WaterMainInfo;
