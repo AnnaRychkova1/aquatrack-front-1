7b572081e81196daf40aae7ac1f96816e9615206
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-23 20:25:47 +0300

    add structure

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,196 @@
-const SignUpForm = () => {
-  return <div>SignUpForm</div>;
-};
+// const SignUpForm = () => {
+//   return <div>SignUpForm</div>;
+// };
 
-export default SignUpForm;
+// export default SignUpForm;
+
+import { Link } from 'react-router-dom';
+import { Logo } from '../Logo';
+import css from './SignUpForm.module.css';
+import React from 'react';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { registerUser, loginUser } from '../../redux/user/userOperations';
+import { useState } from 'react';
+import sprite from '../../assets/sprite.svg';
+import toast from 'react-hot-toast';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('Email field is required'),
+  password: yup
+    .string()
+    .required('Password field is required')
+    .min(6, 'Password must contain at least 6 characters'),
+  repeatPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
+});
+export function SignUpForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const dispatch = useDispatch();
+  const password = React.useRef({});
+  password.current = watch('password', '');
+
+  return (
+    <div className={css.wrapper}>
+      <Logo />
+      <div className={css.formContainer}>
+        <p className={css.title}>Sign Up</p>
+        <form
+          className={css.form}
+          onSubmit={handleSubmit(async data => {
+            try {
+              const resultAction = await dispatch(registerUser(data)).then(
+                res => {
+                  if (res.type === 'auth/register/rejected')
+                    throw new Error(res.payload);
+                  dispatch(loginUser(data));
+                  toast.success('You were successfully signed up!');
+                }
+              );
+
+              if (registerUser.fulfilled.match(resultAction)) {
+                toast.success('You were successfully signed up!');
+                reset();
+              } else if (registerUser.rejected.match(resultAction)) {
+                toast.error('Something went wrong. Please try again.');
+              }
+            } catch (error) {
+              toast.error('Unexpected error. Please try again.');
+            }
+          })}
+        >
+          <label className={css.label}>Email</label>
+          <div className={css.input_field}>
+            <input
+              required={true}
+              className={`${css.input} ${errors.email ? css.error : ''}`}
+              type="email"
+              {...register('email', {
+                pattern: {
+                  value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                  message: 'Please enter valid email',
+                },
+              })}
+              placeholder="Enter your email"
+            />
+            {errors.email && (
+              <span className={css.errors}>{errors.email.message}</span>
+            )}
+          </div>
+          <label className={css.label}>Password</label>
+          <div className={css.input_field}>
+            <input
+              required={true}
+              className={`${css.input} ${errors.password ? css.error : ''}`}
+              type={showPassword ? 'text' : 'password'}
+              {...register('password')}
+              placeholder="Enter your Password"
+            />
+            {errors.password && (
+              <span className={css.errors}>{errors.password.message}</span>
+            )}
+            {!showPassword && (
+              <svg
+                className={css.icon_eye}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <use
+                  width={20}
+                  height={20}
+                  xlinkHref={`${sprite}#icon-eye-off`}
+                ></use>
+              </svg>
+            )}
+
+            {showPassword && (
+              <svg
+                className={css.eyeIconOff}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <use
+                  width={20}
+                  height={20}
+                  xlinkHref={`${sprite}#icon-eye`}
+                ></use>
+              </svg>
+            )}
+          </div>
+          <label className={css.label}>Repeat Password</label>
+          <div className={css.input_field}>
+            <input
+              required={true}
+              className={`${css.input} ${
+                errors.repeatPassword ? css.error : ''
+              }`}
+              type={showPassword ? 'text' : 'password'}
+              {...register('repeatPassword')}
+              placeholder="Repeat Password"
+            />
+            {errors.repeatPassword && (
+              <span className={css.errors}>
+                {errors.repeatPassword.message}
+              </span>
+            )}
+
+            {!showPassword && (
+              <svg
+                className={css.icon_eye}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <use
+                  width={20}
+                  height={20}
+                  xlinkHref={`${sprite}#icon-eye-off`}
+                ></use>
+              </svg>
+            )}
+
+            {showPassword && (
+              <svg
+                className={css.eyeIconOff}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <use
+                  width={20}
+                  height={20}
+                  xlinkHref={`${sprite}#icon-eye`}
+                ></use>
+              </svg>
+            )}
+          </div>
+
+          <button className={css.button} type="submit">
+            Sign Up
+          </button>
+        </form>
+        <p className={css.description}>
+          Already have an account?&nbsp;
+          <Link className={css.link} to={'/signin'}>
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,22 +1,38 @@
-import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
-import Logo from "../../components/Logo/Logo";
-import SignUpForm from "../../components/SignUpForm/SignUpForm";
+// import { Helmet } from "react-helmet-async";
+// import { Link } from "react-router-dom";
+// import Logo from "../../components/Logo/Logo";
+// import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
-const SignUp = () => {
+// const SignUp = () => {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>SignUp</title>
+//       </Helmet>
+//       <Logo />
+//       <SignUpForm />
+//       <p>
+//         Already have account?
+//         <Link to="/signin">Sign In</Link>
+//       </p>
+//     </>
+//   );
+// };
+
+// export default SignUp;
+
+import css from './SignUpPage.module.css';
+
+import { SignUpForm } from '../../components/SignUpForm/SignUpForm';
+import { AdvantagesSection } from '../../components/AdvantagesSection/AdvantagesSection';
+
+export default function SignUpPage() {
   return (
-    <>
-      <Helmet>
-        <title>SignUp</title>
-      </Helmet>
-      <Logo />
+    <div className={css.signUpPageWrapper}>
       <SignUpForm />
-      <p>
-        Already have account?
-        <Link to="/signin">Sign In</Link>
-      </p>
-    </>
+      <div className={css.AdvantagesSection_up}>
+        <AdvantagesSection />
+      </div>
+    </div>
   );
-};
-
-export default SignUp;
+}
