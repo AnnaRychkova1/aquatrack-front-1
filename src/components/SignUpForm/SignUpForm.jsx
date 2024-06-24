d57c6e48b99331c568cce451176b7309ca37d1af
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-25 02:04:26 +0300

    update signup

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -4,6 +4,190 @@
 
 // export default SignUpForm;
 
+// import { Link } from 'react-router-dom';
+// import Logo from '../Logo/Logo';
+// import css from './SignUpForm.module.css';
+// import React from 'react';
+// import * as yup from 'yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import { useForm } from 'react-hook-form';
+// import { useDispatch } from 'react-redux';
+// import { logIn, register } from '../../redux/users/operations';
+// import { useState } from 'react';
+// import sprite from '../../assets/images/svg/symbol-defs.svg';
+// import toast from 'react-hot-toast';
+
+// const schema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .email('Please enter valid email')
+//     .required('Email field is required'),
+//   password: yup
+//     .string()
+//     .required('Password field is required')
+//     .min(6, 'Password must contain at least 6 characters'),
+//   repeatPassword: yup
+//     .string()
+//     .oneOf([yup.ref('password'), null], 'Passwords must match'),
+// });
+
+// const SignUpForm = () => {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const {
+//     register,
+//     handleSubmit,
+//     watch,
+//     formState: { errors },
+//     reset,
+//   } = useForm({
+//     resolver: yupResolver(schema),
+//     mode: 'onChange',
+//     defaultValues: {
+//       email: '',
+//       password: '',
+//     },
+//   });
+
+//   const dispatch = useDispatch();
+//   const password = React.useRef({});
+//   password.current = watch('password', '');
+
+//   return (
+//     <div className={css.loginContainer}>
+//       {<Logo />}
+//       <div className={css.formContainer}>
+//         <p className={css.title}>Sign Up</p>
+//         <form
+//           className={css.form}
+//           onSubmit={handleSubmit(async data => {
+//             try {
+//               const resultAction = await dispatch(register(data)).then(res => {
+//                 if (res.type === 'auth/register/rejected')
+//                   throw new Error(res.payload);
+//                 dispatch(logIn(data));
+//                 toast.success('You were successfully signed up!');
+//               });
+
+//               if (register.fulfilled.match(resultAction)) {
+//                 toast.success('You were successfully signed up!');
+//                 reset();
+//               } else if (register.rejected.match(resultAction)) {
+//                 toast.error('Something went wrong. Please try again.');
+//               }
+//             } catch (error) {
+//               toast.error('Unexpected error. Please try again.');
+//             }
+//           })}
+//         >
+//           <label className={css.label}>Email</label>
+//           <div className={css.input_field}>
+//             <input
+//               required={true}
+//               className={`${css.input} ${errors.email ? css.error : ''}`}
+//               type="email"
+//               {...register('email', {
+//                 pattern: {
+//                   value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+//                   message: 'Please enter valid email',
+//                 },
+//               })}
+//               placeholder="Enter your email"
+//             />
+//             {errors.email && (
+//               <span className={css.errors}>{errors.email.message}</span>
+//             )}
+//           </div>
+//           <label className={css.label}>Password</label>
+//           <div className={css.input_field}>
+//             <input
+//               required={true}
+//               className={`${css.input} ${errors.password ? css.error : ''}`}
+//               type={showPassword ? 'text' : 'password'}
+//               {...register('password')}
+//               placeholder="Enter your password"
+//             />
+//             {errors.password && (
+//               <span className={css.errors}>{errors.password.message}</span>
+//             )}
+//             {!showPassword && (
+//               <svg
+//                 className={css.icon_eye}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#eye-off`}
+//                 ></use>
+//               </svg>
+//             )}
+
+//             {showPassword && (
+//               <svg
+//                 className={css.eyeIconOff}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
+//               </svg>
+//             )}
+//           </div>
+//           <label className={css.label}>Repeat Password</label>
+//           <div className={css.input_field}>
+//             <input
+//               required={true}
+//               className={`${css.input} ${
+//                 errors.repeatPassword ? css.error : ''
+//               }`}
+//               type={showPassword ? 'text' : 'password'}
+//               {...register('repeatPassword')}
+//               placeholder="Repeat password"
+//             />
+//             {errors.repeatPassword && (
+//               <span className={css.errors}>
+//                 {errors.repeatPassword.message}
+//               </span>
+//             )}
+
+//             {!showPassword && (
+//               <svg
+//                 className={css.icon_eye}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#eye-off`}
+//                 ></use>
+//               </svg>
+//             )}
+
+//             {showPassword && (
+//               <svg
+//                 className={css.eyeIconOff}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
+//               </svg>
+//             )}
+//           </div>
+
+//           <button className={css.button} type="submit">
+//             Sign Up
+//           </button>
+//         </form>
+//         <p className={css.description}>
+//           Already have an account?&nbsp;
+//           <Link className={css.link} to={'/signin'}>
+//             Sign In
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUpForm;
+
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import css from './SignUpForm.module.css';
@@ -12,7 +196,7 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { register, logIn } from '../../redux/users/operations';
+import { logIn, register } from '../../redux/users/operations';
 import { useState } from 'react';
 import sprite from '../../assets/images/svg/symbol-defs.svg';
 import toast from 'react-hot-toast';
@@ -54,21 +238,16 @@ const SignUpForm = () => {
 
   return (
     <div className={css.loginContainer}>
-      {<Logo />}
+      <Logo />
       <div className={css.formContainer}>
         <p className={css.title}>Sign Up</p>
         <form
           className={css.form}
           onSubmit={handleSubmit(async data => {
             try {
-              const resultAction = await dispatch(register(data)).then(res => {
-                if (res.type === 'auth/register/rejected')
-                  throw new Error(res.payload);
-                dispatch(logIn(data));
-                toast.success('You were successfully signed up!');
-              });
-
+              const resultAction = await dispatch(register(data));
               if (register.fulfilled.match(resultAction)) {
+                await dispatch(logIn(data));
                 toast.success('You were successfully signed up!');
                 reset();
               } else if (register.rejected.match(resultAction)) {
@@ -109,27 +288,16 @@ const SignUpForm = () => {
             {errors.password && (
               <span className={css.errors}>{errors.password.message}</span>
             )}
-            {!showPassword && (
-              <svg
-                className={css.icon_eye}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                <use
-                  width={20}
-                  height={20}
-                  xlinkHref={`${sprite}#eye-off`}
-                ></use>
-              </svg>
-            )}
-
-            {showPassword && (
-              <svg
-                className={css.eyeIconOff}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
-              </svg>
-            )}
+            <svg
+              className={showPassword ? css.eyeIconOff : css.icon_eye}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <use
+                width={20}
+                height={20}
+                xlinkHref={`${sprite}${showPassword ? '#eye' : '#eye-off'}`}
+              ></use>
+            </svg>
           </div>
           <label className={css.label}>Repeat Password</label>
           <div className={css.input_field}>
@@ -147,30 +315,17 @@ const SignUpForm = () => {
                 {errors.repeatPassword.message}
               </span>
             )}
-
-            {!showPassword && (
-              <svg
-                className={css.icon_eye}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                <use
-                  width={20}
-                  height={20}
-                  xlinkHref={`${sprite}#eye-off`}
-                ></use>
-              </svg>
-            )}
-
-            {showPassword && (
-              <svg
-                className={css.eyeIconOff}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
-              </svg>
-            )}
+            <svg
+              className={showPassword ? css.eyeIconOff : css.icon_eye}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <use
+                width={20}
+                height={20}
+                xlinkHref={`${sprite}${showPassword ? '#eye' : '#eye-off'}`}
+              ></use>
+            </svg>
           </div>
-
           <button className={css.button} type="submit">
             Sign Up
           </button>
--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,3 +1,123 @@
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://aquatrack-back-1.onrender.com/';
+
+// const setAuthHeader = token => {
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
+//------
+//використовує інтерцептор axios для автоматичного оновлення токену
+//доступу(accessToken) при отриманні відповіді з кодом помилки 401(неавторизовано).
+///---
+
+// axios.interceptors.response.use(
+//   res => res,
+//   async err => {
+//     const originalRequest = err.config;
+
+//     if (err.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+
+//       const refreshToken = localStorage.getItem('refreshToken');
+
+//       if (refreshToken.length > 0) {
+//         try {
+//           const res = await axios.post('/users/refresh', { refreshToken });
+
+//           SetAuthHeader(res.data.accessToken);
+//           localStorage.setItem('refreshToken', res.data.refreshToken);
+//           originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+//           return axios(originalRequest);
+//         } catch (refreshError) {
+//           localStorage.setItem('refreshToken', '');
+//           ClearAuthHeader();
+//           return Promise.reject(refreshError);
+//         }
+//       }
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export const register = createAsyncThunk(
+//   'auth/register',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       const res = await axios.post(
+//         'https://aquatrack-back-1.onrender.com/users',
+//         credentials
+//       );
+//       setAuthHeader(res.data.token);
+//       return res.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+//------------------
+// export const logIn = createAsyncThunk(
+//   'auth/login',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       const res = await axios.post('/users/login', credentials);
+//       setAuthHeader(res.data.token);
+//       return res.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+//-------------------
+// export const logIn = createAsyncThunk(
+//   'auth/login',
+//   async ({ email, password }, thunkAPI) => {
+//     try {
+//       const res = await axios.post(
+//         'https://aquatrack-back-1.onrender.com/users',
+//         { email, password }
+//       );
+//       SetAuthHeader(res.data.accessToken);
+
+//       return res.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+//   try {
+//     await axios.post('https://aquatrack-back-1.onrender.com/users');
+//     clearAuthHeader();
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message);
+//   }
+// });
+
+// export const refreshUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, thunkAPI) => {
+//     const state = thunkAPI.getState();
+//     const persistedToken = state.auth.token;
+//     if (persistedToken === null) {
+//       return thunkAPI.rejectWithValue('Unable to fetch user');
+//     }
+//     try {
+//       setAuthHeader(persistedToken);
+//       const res = await axios.get('/users/current');
+//       return res.data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
@@ -11,31 +131,35 @@ const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
-//використовує інтерцептор axios для автоматичного оновлення токену
-//доступу(accessToken) при отриманні відповіді з кодом помилки 401(неавторизовано).
+// Використання інтерцептора axios для автоматичного оновлення токену
+// доступу (accessToken) при отриманні відповіді з кодом помилки 401 (неавторизовано).
 
 axios.interceptors.response.use(
   res => res,
   async err => {
     const originalRequest = err.config;
 
-    if (err.response.status === 401 && !originalRequest._retry) {
+    if (
+      err.response &&
+      err.response.status === 401 &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true;
 
       const refreshToken = localStorage.getItem('refreshToken');
 
-      if (refreshToken.length > 0) {
+      if (refreshToken && refreshToken.length > 0) {
         try {
           const res = await axios.post('/users/refresh', { refreshToken });
 
-          SetAuthHeader(res.data.accessToken);
+          setAuthHeader(res.data.accessToken);
           localStorage.setItem('refreshToken', res.data.refreshToken);
           originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
 
           return axios(originalRequest);
         } catch (refreshError) {
-          localStorage.setItem('refreshToken', '');
-          ClearAuthHeader();
+          localStorage.removeItem('refreshToken');
+          clearAuthHeader();
           return Promise.reject(refreshError);
         }
       }
@@ -60,19 +184,6 @@ export const register = createAsyncThunk(
   }
 );
 
-// export const logIn = createAsyncThunk(
-//   'auth/login',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const res = await axios.post('/users/login', credentials);
-//       setAuthHeader(res.data.token);
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
 export const logIn = createAsyncThunk(
   'auth/login',
   async ({ email, password }, thunkAPI) => {
@@ -81,8 +192,7 @@ export const logIn = createAsyncThunk(
         'https://aquatrack-back-1.onrender.com/users',
         { email, password }
       );
-      SetAuthHeader(res.data.accessToken);
-
+      setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -104,7 +214,7 @@ export const refreshUser = createAsyncThunk(
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
-    if (persistedToken === null) {
+    if (!persistedToken) {
       return thunkAPI.rejectWithValue('Unable to fetch user');
     }
     try {
