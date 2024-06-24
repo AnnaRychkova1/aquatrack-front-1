ce398ed7a5bddffc1b9f324efc1b9b61e086fea6
Yuriy2223 <yuriy.shukan@gmail.com>
2024-06-24 14:46:40 +0300

    Update WaterForm.jsx

--- src/modals/WaterForm/WaterForm.jsx
+++ src/modals/WaterForm/WaterForm.jsx
@@ -1,9 +1,9 @@
-// import UniversalModal from '../Modal/Modal';
+import UniversalModal from '../Modal/Modal';
 import css from './WaterForm.module.css';
 import Iconsvg from '../../components/Icon/Icon';
 import { useState } from 'react';
 
-const WaterForm = () => {
+const WaterForm = ({ isOpen, closeModal }) => {
   const [number, setNumber] = useState(50);
   const [maxValue, setMaxValue] = useState(0);
   const [minValue, setMinValue] = useState(0);
@@ -23,54 +23,56 @@ const WaterForm = () => {
   };
 
   return (
-    // <UniversalModal>
-    <form className={css.waterForm}>
-      <h3 className={css.title}>Add water</h3>
-      <p className={css.subtitle}>Choose a value</p>
-      <p className={css.amountWaterLabel}>Amount of water:</p>
-      <div className={css.btnBox}>
-        <button className={css.btnReduce} onClick={decrementNumber}>
-          <Iconsvg
-            width="18"
-            height="18"
-            iconName="minus"
-            className={css.btnSvg}
-          />
+    <UniversalModal isOpen={isOpen} closeModal={closeModal}>
+      <form className={css.waterForm}>
+        <h3 className={css.title}>Add water</h3>
+        <p className={css.subtitle}>Choose a value</p>
+        <p className={css.amountWaterLabel}>Amount of water:</p>
+        <div className={css.btnBox}>
+          <button className={css.btnReduce} onClick={decrementNumber}>
+            <Iconsvg
+              width="18"
+              height="18"
+              iconName="minus"
+              className={css.btnSvg}
+            />
+          </button>
+          <input className={css.input1} type="number" value={number} readOnly />
+          <span className={css.ml}>ml</span>
+          <button className={css.btnZoom} onClick={incrementNumber}>
+            <Iconsvg
+              width="18"
+              height="18"
+              iconName="plus1"
+              className={css.btnSvg}
+            />
+          </button>
+        </div>
+        <p className={css.timeLabel}>Recording time:</p>
+
+        <input
+          className={css.inputTime}
+          type="time"
+          id="myTime"
+          value="07:00"
+        ></input>
+
+        <p className={css.valueLabel}>Enter the value of the water used:</p>
+
+        <input
+          className={css.input2}
+          type="number"
+          value={number}
+          onChange={e =>
+            setNumber(Math.min(Math.max(e.target.value, 50), 5000))
+          }
+        />
+
+        <button className={css.btnSave} type="submit">
+          Save
         </button>
-        <input className={css.input1} type="number" value={number} readOnly />
-        <span className={css.ml}>ml</span>
-        <button className={css.btnZoom} onClick={incrementNumber}>
-          <Iconsvg
-            width="18"
-            height="18"
-            iconName="plus1"
-            className={css.btnSvg}
-          />
-        </button>
-      </div>
-      <p className={css.timeLabel}>Recording time:</p>
-
-      <input
-        className={css.inputTime}
-        type="time"
-        id="myTime"
-        value="07:00"
-      ></input>
-
-      <p className={css.valueLabel}>Enter the value of the water used:</p>
-
-      <input
-        className={css.input2}
-        type="number"
-        value={number}
-        onChange={e => setNumber(Math.min(Math.max(e.target.value, 50), 5000))}
-      />
-
-      <button className={css.btnSave} type="submit">
-        Save
-      </button>
-    </form>
-    // </UniversalModal>
+      </form>
+    </UniversalModal>
   );
 };
 
