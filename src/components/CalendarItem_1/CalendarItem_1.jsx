9c78ffe53312028815b55c8d35287e4152a70b94
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-06-24 14:56:14 +0300

    Update CalendarItem_1.jsx

--- src/components/CalendarItem_1/CalendarItem_1.jsx
+++ src/components/CalendarItem_1/CalendarItem_1.jsx
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux';
+import { selectDate } from '../../redux/date/selectors';
 import css from './CalendarItem_1.module.css';
+
 const CalendarItem_1 = ({ day, percent }) => {
+  const storeDate = new Date(useSelector(selectDate));
+  const dateNumber = storeDate.getDate();
+  const numericDay = parseInt(day, 10);
   const numericPercent = parseInt(percent.replace('%', ''), 10);
   return (
     <div className={css.dayInfo}>
-      {numericPercent < 100 ? (
+      {numericDay === dateNumber ? (
+        <p className={css.dayNumberCurrent}>{day}</p>
+      ) : numericPercent < 100 ? (
         <p className={css.dayNumberPart}>{day}</p>
       ) : (
         <p className={css.dayNumberFull}>{day}</p>
