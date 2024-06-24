0e8b95a5d633e1e97404374d2624ddcc01aecaf9
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-24 19:22:34 +0300

    update css

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://aquatrack-back-1.onrender.com/';
+axios.defaults.baseURL = 'https://aquatrack-back-1.onrender.com/';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+//axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
