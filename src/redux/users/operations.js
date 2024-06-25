69f85b28c98653e8bcfdd4c85970a8f013b19f3d
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-06-25 09:26:21 +0300

    redux

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,123 +1,3 @@
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-
-// axios.defaults.baseURL = 'https://aquatrack-back-1.onrender.com/';
-
-// const setAuthHeader = token => {
-//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-// };
-
-// const clearAuthHeader = () => {
-//   axios.defaults.headers.common.Authorization = '';
-// };
-//------
-//використовує інтерцептор axios для автоматичного оновлення токену
-//доступу(accessToken) при отриманні відповіді з кодом помилки 401(неавторизовано).
-///---
-
-// axios.interceptors.response.use(
-//   res => res,
-//   async err => {
-//     const originalRequest = err.config;
-
-//     if (err.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-
-//       const refreshToken = localStorage.getItem('refreshToken');
-
-//       if (refreshToken.length > 0) {
-//         try {
-//           const res = await axios.post('/users/refresh', { refreshToken });
-
-//           SetAuthHeader(res.data.accessToken);
-//           localStorage.setItem('refreshToken', res.data.refreshToken);
-//           originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
-
-//           return axios(originalRequest);
-//         } catch (refreshError) {
-//           localStorage.setItem('refreshToken', '');
-//           ClearAuthHeader();
-//           return Promise.reject(refreshError);
-//         }
-//       }
-//     }
-//     return Promise.reject(err);
-//   }
-// );
-
-// export const register = createAsyncThunk(
-//   'auth/register',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const res = await axios.post(
-//         'https://aquatrack-back-1.onrender.com/users',
-//         credentials
-//       );
-//       setAuthHeader(res.data.token);
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-//------------------
-// export const logIn = createAsyncThunk(
-//   'auth/login',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const res = await axios.post('/users/login', credentials);
-//       setAuthHeader(res.data.token);
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-//-------------------
-// export const logIn = createAsyncThunk(
-//   'auth/login',
-//   async ({ email, password }, thunkAPI) => {
-//     try {
-//       const res = await axios.post(
-//         'https://aquatrack-back-1.onrender.com/users',
-//         { email, password }
-//       );
-//       SetAuthHeader(res.data.accessToken);
-
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-//   try {
-//     await axios.post('https://aquatrack-back-1.onrender.com/users');
-//     clearAuthHeader();
-//   } catch (error) {
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// });
-
-// export const refreshUser = createAsyncThunk(
-//   'auth/refresh',
-//   async (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
-//     if (persistedToken === null) {
-//       return thunkAPI.rejectWithValue('Unable to fetch user');
-//     }
-//     try {
-//       setAuthHeader(persistedToken);
-//       const res = await axios.get('/users/current');
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
@@ -167,12 +47,15 @@ axios.interceptors.response.use(
     return Promise.reject(err);
   }
 );
+// -------
+//----------
 
 export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
     try {
       const res = await axios.post(
+        //  ('/users/signup', credentials);
         'https://aquatrack-back-1.onrender.com/users',
         credentials
       );
@@ -186,11 +69,12 @@ export const register = createAsyncThunk(
 
 export const logIn = createAsyncThunk(
   'auth/login',
-  async ({ email, password }, thunkAPI) => {
+  async (credentials, thunkAPI) => {
     try {
       const res = await axios.post(
-        'https://aquatrack-back-1.onrender.com/users',
-        { email, password }
+        '/users/login',
+        // 'https://aquatrack-back-1.onrender.com/users',
+        credentials
       );
       setAuthHeader(res.data.token);
       return res.data;
@@ -202,7 +86,8 @@ export const logIn = createAsyncThunk(
 
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('https://aquatrack-back-1.onrender.com/users');
+    await axios.post('/users/logout');
+    // ('https://aquatrack-back-1.onrender.com/users');
     clearAuthHeader();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
