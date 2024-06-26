a929f6684c9d57ecbc1627ea34178d11aade8510
Agletty <agletty.lu@gmail.com>
2024-06-26 11:35:40 +0300

    add DailyNorm

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,13 @@
 import css from './WaterDailyNorma.module.css';
+// import { useSelector } from 'react-redux';
+// import { selectQuantity } from '../../redux/water/selectors.js';
 
 const WaterDailyNorma = () => {
+  // const daylyNorm = useSelector(selectQuantity);
   return (
     <div className={css.contDailyNorma}>
-      <p className={css.dailyNorma}>1.5 L</p>
+      {/* <p className={css.dailyNorma}>{daylyNorm ? `${daylyNorm}L` : '0L'}</p> */}
+      <p className={css.dailyNorma}>1.5L</p>
       <p className={css.textDailyNorma}>My daili norma</p>
     </div>
   );
--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,8 +1,27 @@
 import css from './WaterProgressBar.module.css';
 import Iconsvg from '../Icon/Icon';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import { selectDate } from '../../redux/date/selectors';
+
+// import {
+//   selectQuantity,
+//   selectDateWater,
+// } from '../../redux/water/selectors.js'; /*для отримання даних про кількість води і дату води.*/
+// import { getDayWater } from '../../redux/water/operations.js'; /* для отримання даних про воду для певного дня.*/
 
 const WaterProgressBar = () => {
+  //   const dayWater = useSelector(selectDateWater);
+  //   const dayNorma = useSelector(selectQuantity);
+
+  //   // Дата зі стору
+  //   const storeDate = new Date(useSelector(selectDate));
+  //   const storeDay = storeDate.getDate();
+
+  //   // Дата з календаря
+  //   const сalendarDate = new Date(day);
+  //   const сalendarDay = сalendarDate.getDate();
+
   const [completed, setCompleted] = useState(50);
 
   useEffect(() => {
@@ -25,6 +44,10 @@ const WaterProgressBar = () => {
 
   return (
     <div className={css.contBar}>
+      {/* <p className={css.textBar}>
+        {storeDay === сalendarDay ? 'Today' : сalendarDay}
+      </p> */}
+
       <p className={css.textBar}>Today</p>
       <div className={css.waterProgressBarWrapper}>
         <div className={css.waterProgressBar}>
