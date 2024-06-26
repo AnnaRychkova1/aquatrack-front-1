2bf3128d062a737451e3125a25a6bbdd4660d94b
Yuriy2223 <yuriy.shukan@gmail.com>
2024-06-26 03:56:40 +0300

    123

--- src/modals/WaterForm/WaterForm.jsx
+++ src/modals/WaterForm/WaterForm.jsx
@@ -1,95 +1,9 @@
-// import css from './WaterForm.module.css';
-// import Iconsvg from '../../components/Icon/Icon';
-// import * as yup from 'yup';
-// import { useState } from 'react';
-
-// const schemaWaterForm = yup.object().shape({
-//   waterAmount: yup
-//     .number()
-//     .typeError('Please enter a valid number')
-//     .min(50, 'Minimum value is 50')
-//     .max(5000, 'Maximum value is 5000')
-//     .required('Water amount is required'),
-//   recordingTime: yup.string().required('Recording time is required'),
-// });
-
-// const WaterForm = () => {
-//   const [number, setNumber] = useState(50);
-//   const [maxValue, setMaxValue] = useState(0);
-//   const [minValue, setMinValue] = useState(0);
-
-//   const incrementNumber = e => {
-//     e.preventDefault();
-//     const newNumber = Math.min(number + 50, 5000);
-//     setNumber(newNumber);
-//     setMaxValue(Math.max(maxValue, newNumber));
-//   };
-
-//   const decrementNumber = e => {
-//     e.preventDefault();
-//     const newNumber = Math.max(number - 50, 50);
-//     setNumber(newNumber);
-//     setMinValue(Math.min(minValue, newNumber));
-//   };
-
-//   return (
-//     <form className={css.waterForm}>
-//       <p className={css.amountWaterLabel}>Amount of water:</p>
-//       <div className={css.btnBox}>
-//         <button className={css.btnReduce} onClick={decrementNumber}>
-//           <Iconsvg
-//             width="18"
-//             height="18"
-//             iconName="minus"
-//             className={css.btnSvg}
-//           />
-//         </button>
-//         <input className={css.input1} type="number" value={number} readOnly />
-//         <span className={css.ml}>ml</span>
-//         <button className={css.btnZoom} onClick={incrementNumber}>
-//           <Iconsvg
-//             width="18"
-//             height="18"
-//             iconName="plus"
-//             className={css.btnSvg}
-//           />
-//         </button>
-//       </div>
-//       <p className={css.timeLabel}>Recording time:</p>
-
-//       <input className={css.inputTime} type="time" value="07:00"></input>
-
-//       <p className={css.valueLabel}>Enter the value of the water used:</p>
-
-//       <input
-//         className={css.input2}
-//         type="number"
-//         value={number}
-//         onChange={e => setNumber(Math.min(Math.max(e.target.value, 50), 5000))}
-//       />
-
-//       <button className={css.btnSave} type="submit">
-//         Save
-//       </button>
-//     </form>
-//   );
-// };
-
-// export default WaterForm;
-
-/**проба 2 */
-
 import css from './WaterForm.module.css';
 import Iconsvg from '../../components/Icon/Icon';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
-import {
-  addWater,
-  updateWater,
-  closeModal,
-  waterId,
-} from '../../redux/water/operations.js';
+import { addWater, updateWater } from '../../redux/water/operations.js';
 // import * as yup from 'yup';
 // import { useForm } from 'react-hook-form';
 
@@ -103,7 +17,12 @@ import {
 //   recordingTime: yup.string().required('Recording time is required'),
 // });
 
-const WaterForm = ({ operationType }) => {
+const WaterForm = ({
+  operationType,
+  closeModal,
+  waterId,
+  //  volume
+}) => {
   const dispatch = useDispatch();
   // const { register, handleSubmit, errors } = useForm({
   //   validationSchema: schemaWaterForm,
@@ -158,8 +77,7 @@ const WaterForm = ({ operationType }) => {
             className={css.btnSvg}
           />
         </button>
-        <input className={css.input1} type="number" value={number} readOnly />
-        <span className={css.ml}>ml</span>
+        <span className={css.numberMl}>{number} ml</span>
         <button className={css.btnZoom} onClick={incrementNumber}>
           <Iconsvg
             width="18"
@@ -200,7 +118,12 @@ export default WaterForm;
       </p> */
 }
 
-// код 2
+ // function myProps() {
+  //   const isEdit = operationType === 'edit';
+  //   const initialWaterAmount = isEdit ? { volume } : 50;
+  //   const [number, setNumber] = useState(initialWaterAmount);
+  // }
+
 
 // import  { useState } from 'react';
 // import { useForm } from 'react-hook-form';
