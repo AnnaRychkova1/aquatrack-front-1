20b55826027ee9d12388ee0a43afedc256ccd6bc
Petro Svist <svist.petro@gmail.com>
2024-06-30 13:10:04 +0300

    Добавив стилі та відображення статистики

--- src/redux/date/selectors.js
+++ src/redux/date/selectors.js
@@ -1,3 +1,2 @@
 export const selectDate = state => state.date.date;
 export const paginationDate = state => state.date.paginationDate;
-export const paginationBtnDisabled = state => state.date.paginationBtnDisabled;
