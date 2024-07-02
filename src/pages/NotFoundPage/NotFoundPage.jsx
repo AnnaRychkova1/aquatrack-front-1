a0a65008e005f9e487da0f8e7eb8c084726462af
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-07-02 18:23:32 +0300

    add notfoundpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
+import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className={css.generalHomePage}>
       <Helmet>
         <title>Page not found</title>
       </Helmet>
-      <p>Sorry, the page you visited does not exist!</p>
-      <button>
+      <p className={css.textPage}>
+        Sorry, the page you visited does not exist!
+      </p>
+      <button className={css.btnPage}>
         <Link to="/">Back home</Link>
       </button>
     </div>
