637b2ac353d1d7795524a8654bd33f6c91ff5446
Yuriy2223 <yuriy.shukan@gmail.com>
2024-07-09 02:28:34 +0300

    corrected form

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -29,6 +29,7 @@ const SignInForm = () => {
     handleSubmit,
     formState: { errors },
     reset,
+    clearErrors,
   } = useForm({
     resolver: yupResolver(schema),
     mode: 'onBlur',
@@ -37,6 +38,8 @@ const SignInForm = () => {
       password: '',
     },
   });
+
+  const handleFocus = fieldName => clearErrors(fieldName);
 
   const onSubmit = data => {
     dispatch(logIn(data));
@@ -62,6 +65,7 @@ const SignInForm = () => {
               })}
               placeholder="Enter your email"
               autoComplete="on"
+              onFocus={() => handleFocus('email')}
             />
             {errors.email && (
               <span className={css.errors}>{errors.email.message}</span>
@@ -74,6 +78,7 @@ const SignInForm = () => {
               type={showPassword ? 'text' : 'password'}
               {...register('password')}
               placeholder="Enter your password"
+              onFocus={() => handleFocus('password')}
             />
             {errors.password && (
               <span className={css.errors}>{errors.password.message}</span>
@@ -116,3 +121,124 @@ const SignInForm = () => {
   );
 };
 export default SignInForm;
+
+/**=================================================== */
+
+// import { NavLink } from 'react-router-dom';
+// import Logo from '../Logo/Logo';
+// import * as yup from 'yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import { useForm } from 'react-hook-form';
+// import { useDispatch } from 'react-redux';
+// import { logIn } from '../../redux/users/operations';
+// import { useState } from 'react';
+// import sprite from '../../assets/images/svg/symbol-defs.svg';
+// import css from './SignInForm.module.css';
+
+// const schema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .email('Please enter valid email')
+//     .required('Email field is required'),
+//   password: yup
+//     .string()
+//     .required('Password field is required')
+//     .min(6, 'Password must contain at least 6 characters'),
+// });
+
+// const SignInForm = () => {
+//   const dispatch = useDispatch();
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//     reset,
+//   } = useForm({
+//     resolver: yupResolver(schema),
+//     mode: 'onBlur',
+//     defaultValues: {
+//       email: '',
+//       password: '',
+//     },
+//   });
+
+//   const onSubmit = data => {
+//     dispatch(logIn(data));
+//     reset();
+//   };
+
+//   return (
+//     <div className={css.loginContainer}>
+//       {<Logo />}
+//       <div className={css.formContainer}>
+//         <h1 className={css.title}>Sign In</h1>
+//         <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
+//           <label className={css.label}>Email</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.email ? css.error : ''}`}
+//               type="email"
+//               {...register('email', {
+//                 pattern: {
+//                   value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+//                   message: 'Please enter valid email',
+//                 },
+//               })}
+//               placeholder="Enter your email"
+//               autoComplete="on"
+//             />
+//             {errors.email && (
+//               <span className={css.errors}>{errors.email.message}</span>
+//             )}
+//           </div>
+//           <label className={css.label}>Password</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.password ? css.error : ''}`}
+//               type={showPassword ? 'text' : 'password'}
+//               {...register('password')}
+//               placeholder="Enter your password"
+//             />
+//             {errors.password && (
+//               <span className={css.errors}>{errors.password.message}</span>
+//             )}
+//             {!showPassword && (
+//               <svg
+//                 className={css.icon_eye}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#eye-off`}
+//                 ></use>
+//               </svg>
+//             )}
+
+//             {showPassword && (
+//               <svg
+//                 className={css.eyeIconOff}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
+//               </svg>
+//             )}
+//           </div>
+
+//           <button className={css.button} type="submit">
+//             Sign In
+//           </button>
+//         </form>
+//         <p className={css.description}>
+//           Don’t have an account?&nbsp;
+//           <NavLink className={css.link} to={'/signup'}>
+//             Sign Up
+//           </NavLink>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+// export default SignInForm;
