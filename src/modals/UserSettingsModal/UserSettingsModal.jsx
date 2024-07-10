09fd01339dc5281c862b28d865df6c23b21d3c22
AnnaRychkova1 <aanytkaa@gmail.com>
2024-07-10 14:17:35 +0100

    setModal

--- src/modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/modals/UserSettingsModal/UserSettingsModal.jsx
@@ -1,4 +1,3 @@
-// import { useEffect } from 'react';
 import UserSettingsForm from '../../components/UserSettingsForm/UserSettingsForm';
 import UniversalModal from '../Modal/Modal';
 import css from './UserSettingsModal.module.css';
@@ -6,7 +5,7 @@ const UserSettingsModal = ({ isOpen, closeModal, closePopover }) => {
   const addModalClassName = isOpen ? css.settingModal : '';
   const addOverlayClassName = isOpen ? css.settingOverlay : '';
   return (
-    <div className={css.settingsScroll}>
+    <div className={css.container}>
       <UniversalModal
         isOpen={isOpen}
         closeModal={closeModal}
@@ -14,14 +13,12 @@ const UserSettingsModal = ({ isOpen, closeModal, closePopover }) => {
         addOverlayClassName={addOverlayClassName}
       >
         <div className={css.settingsWrapper}>
-          {/* <div className={css.settingsScroll}> */}
           <h2 className={css.settingsTitle}>Setting</h2>
           <UserSettingsForm
             closeModal={closeModal}
             closePopover={closePopover}
           />
         </div>
-        {/* </div> */}
       </UniversalModal>
     </div>
   );
