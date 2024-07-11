2704ab9a248c5bd1ea240327006c49b3b5a962a7
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-07-11 11:13:35 +0300

    add

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,3 +1,126 @@
+// import { NavLink } from 'react-router-dom';
+// import Logo from '../Logo/Logo';
+// import * as yup from 'yup';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import { useForm } from 'react-hook-form';
+// import { useDispatch } from 'react-redux';
+// import { logIn } from '../../redux/users/operations';
+// import { useState } from 'react';
+// import sprite from '../../assets/images/svg/symbol-defs.svg';
+// import css from './SignInForm.module.css';
+// import GoogleBtn from '../GoogleBtn/GoogleBtn';
+
+// const schema = yup.object().shape({
+//   email: yup
+//     .string()
+//     .email('Please enter valid email')
+//     .required('Email field is required'),
+//   password: yup
+//     .string()
+//     .required('Password field is required')
+//     .min(6, 'Password must contain at least 6 characters'),
+// });
+
+// const SignInForm = () => {
+//   const dispatch = useDispatch();
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//     reset,
+//   } = useForm({
+//     resolver: yupResolver(schema),
+//     mode: 'onBlur',
+//     defaultValues: {
+//       email: '',
+//       password: '',
+//     },
+//   });
+
+//   const onSubmit = data => {
+//     dispatch(logIn(data));
+//     reset();
+//   };
+
+//   return (
+//     <div className={css.loginContainer}>
+//       {<Logo />}
+//       <div className={css.formContainer}>
+//         <h1 className={css.title}>Sign In</h1>
+//         <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
+//           <label className={css.label}>Email</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.email ? css.error : ''}`}
+//               type="email"
+//               {...register('email', {
+//                 pattern: {
+//                   value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+//                   message: 'Please enter valid email',
+//                 },
+//               })}
+//               placeholder="Enter your email"
+//               autoComplete="on"
+//             />
+//             {errors.email && (
+//               <span className={css.errors}>{errors.email.message}</span>
+//             )}
+//           </div>
+//           <label className={css.label}>Password</label>
+//           <div className={css.input_field}>
+//             <input
+//               className={`${css.input} ${errors.password ? css.error : ''}`}
+//               type={showPassword ? 'text' : 'password'}
+//               {...register('password')}
+//               placeholder="Enter your password"
+//             />
+//             {errors.password && (
+//               <span className={css.errors}>{errors.password.message}</span>
+//             )}
+//             {!showPassword && (
+//               <svg
+//                 className={css.icon_eye}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use
+//                   width={20}
+//                   height={20}
+//                   xlinkHref={`${sprite}#eye-off`}
+//                 ></use>
+//               </svg>
+//             )}
+
+//             {showPassword && (
+//               <svg
+//                 className={css.eyeIconOff}
+//                 onClick={() => setShowPassword(!showPassword)}
+//               >
+//                 <use width={20} height={20} xlinkHref={`${sprite}#eye`}></use>
+//               </svg>
+//             )}
+//           </div>
+
+//           <button className={css.button} type="submit">
+//             Sign In
+//           </button>
+//         </form>
+
+//         <GoogleBtn type="Up" />
+
+//         <p className={css.description}>
+//           Don’t have an account?&nbsp;
+//           <NavLink className={css.link} to={'/signup'}>
+//             Sign Up
+//           </NavLink>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+// export default SignInForm;
+
 import { NavLink } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import * as yup from 'yup';
@@ -5,10 +128,11 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/users/operations';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import sprite from '../../assets/images/svg/symbol-defs.svg';
 import css from './SignInForm.module.css';
 import GoogleBtn from '../GoogleBtn/GoogleBtn';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const schema = yup.object().shape({
   email: yup
@@ -24,6 +148,8 @@ const schema = yup.object().shape({
 const SignInForm = () => {
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const {
     register,
@@ -39,6 +165,16 @@ const SignInForm = () => {
     },
   });
 
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const token = query.get('token');
+
+    if (token) {
+      localStorage.setItem('token', token);
+      navigate('/tracker'); // Redirect to tracker if token is present
+    }
+  }, [location, navigate]);
+
   const onSubmit = data => {
     dispatch(logIn(data));
     reset();
@@ -46,7 +182,7 @@ const SignInForm = () => {
 
   return (
     <div className={css.loginContainer}>
-      {<Logo />}
+      <Logo />
       <div className={css.formContainer}>
         <h1 className={css.title}>Sign In</h1>
         <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
@@ -55,12 +191,7 @@ const SignInForm = () => {
             <input
               className={`${css.input} ${errors.email ? css.error : ''}`}
               type="email"
-              {...register('email', {
-                pattern: {
-                  value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-                  message: 'Please enter valid email',
-                },
-              })}
+              {...register('email')}
               placeholder="Enter your email"
               autoComplete="on"
             />
@@ -107,7 +238,7 @@ const SignInForm = () => {
           </button>
         </form>
 
-        <GoogleBtn type="Up" />
+        <GoogleBtn type="In" />
 
         <p className={css.description}>
           Don’t have an account?&nbsp;
@@ -119,4 +250,5 @@ const SignInForm = () => {
     </div>
   );
 };
+
 export default SignInForm;
---
+++ src/components/Statistic/Statistic 2.jsx
@@ -0,0 +1,128 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+} from 'recharts';
+import css from './Statistic.module.css';
+import { format, subDays, getMonth, getYear } from 'date-fns';
+import { paginationDate } from '../../redux/date/selectors';
+import { selectToken } from '../../redux/users/selectors';
+import { fetchMonthlyWater } from '../../redux/water/operations';
+import { selectDate } from '../../redux/date/selectors';
+
+const Statistic = ({ data }) => {
+  const storePaginationDate = new Date(useSelector(paginationDate));
+  const selectedDate = useSelector(selectDate);
+
+  const month = getMonth(storePaginationDate) + 1;
+  const year = getYear(storePaginationDate);
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchMonthlyWater({ month, year, token }));
+    }
+  }, [dispatch, month, year, token]);
+
+  const waterPortions = data.map(el => {
+    return { date: format(el.date, 'dd.MM.yyyy'), volume: el.volume / 1000 };
+  });
+
+  const arrayDates = [];
+  for (let i = 6; i >= 0; i--) {
+    arrayDates.push(format(subDays(selectedDate, i), 'dd.MM.yyyy'));
+  }
+
+  const finalData = arrayDates.map(date => {
+    let a = 0;
+    waterPortions.forEach(el => {
+      if (date === el.date) {
+        a += el.volume;
+      }
+    });
+
+    if (a > 0) {
+      a = a.toFixed(3);
+    }
+
+    return { date: date.split('.')[0], Water: a };
+  });
+
+  const gradientId = 'waterGradient';
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className={css.customTooltip}>
+          <p className={css.water}>{`${payload[0].value * 1000} ml`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+  const formatYAxis = tickItem => {
+    if (tickItem === 0) {
+      return '0';
+    }
+    return `${tickItem} L`;
+  };
+
+  return (
+    <div className={css.statistics}>
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={finalData}>
+          <defs>
+            <linearGradient id={gradientId} x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#9BE1A0" stopOpacity={1} />
+              <stop offset="100%" stopColor="#9BE1A0" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3" stroke="none" />
+          <XAxis
+            dataKey="date"
+            padding={{ left: 20, right: 20 }}
+            tickSize={false}
+            tickLine={false}
+            tick={{ fill: 'var(--main-text)' }}
+            stroke=""
+          />
+          <YAxis
+            padding={{ top: 20, bottom: 20 }}
+            tickSize={false}
+            tickLine={false}
+            tick={{ fill: 'var(--main-text)' }}
+            stroke=""
+            tickFormatter={formatYAxis}
+          />
+          <Area
+            dataKey="Water"
+            dot={{
+              stroke: '#87D28D',
+              strokeWidth: 3,
+              r: 9,
+              fill: '#fff',
+              fillOpacity: '1',
+            }}
+            stroke="#87D28D"
+            strokeWidth={3}
+            fill={`url(#${gradientId})`}
+          />
+          <Tooltip
+            content={<CustomTooltip />}
+            cursor={{ stroke: '' }}
+            position={{ y: 0 }}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Statistic;
