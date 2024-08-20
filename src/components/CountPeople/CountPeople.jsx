f88303c50dfab3856baa99d5888456a30e1b2043
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-08-20 10:22:50 +0100

    users

--- src/components/CountPeople/CountPeople.jsx
+++ src/components/CountPeople/CountPeople.jsx
@@ -17,14 +17,13 @@ const CountPeople = () => {
   const isLoading = useSelector(selectIsLoading);
   const isError = useSelector(selectIsError);
   const userCount = useSelector(selectCount);
-  console.log(userCount);
 
   const formatUserCount = userCount => {
     if (userCount !== null) {
       if (userCount > 10000) {
         return `${(userCount / 1000).toFixed(2)}k`;
       } else {
-        return userCount;
+        return userCount.toString();
       }
     }
     return '';
