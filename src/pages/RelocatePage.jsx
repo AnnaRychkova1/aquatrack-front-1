01964f106a0ab8194312916cba6f412a4682a211
Anna Rychkova <aanytkaa@gmail.com>
2025-02-21 19:07:02 +0000

    relocaring

--- src/pages/RelocatePage.jsx
+++ src/pages/RelocatePage.jsx
@@ -1,28 +1,41 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../shared/components/Loader/Loader';
 import { currentUser } from '../redux/users/operations';
+import { selectIsLoading, selectUser } from '../redux/users/selectors'; // Додано селектори
 
 const RelocatePage = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const query = new URLSearchParams(location.search);
   const token = query.get('token');
-  console.log('token1', token);
+
+  const isLoading = useSelector(selectIsLoading);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     if (token) {
+      console.log(token);
       localStorage.setItem('token', token);
+      console.log('go to dispatch');
       dispatch(currentUser());
     }
-  }, [location, dispatch, token]);
+  }, [token, dispatch]);
 
-  return (
-    <>
-      <Loader />
-    </>
-  );
+  useEffect(() => {
+    if (user) {
+      navigate('/tracker');
+    }
+  }, [user, navigate]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return null;
 };
 
 export default RelocatePage;
